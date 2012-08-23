861695e9e56a9b55d2e3a8b3aa22c4fce3a88f02
Sem <sembrestels@riseup.net>
2012-08-23 18:05:24 +0200

    Readded excerpt in river item.

--- views/default/river/object/crud/create.php
+++ views/default/river/object/crud/create.php
@@ -1,11 +1,13 @@
 <?php
 /**
- * Assembly river view.
+ * Crud river view.
  */
 
 $object = $vars['item']->getObjectEntity();
+$excerpt = strip_tags($object->description);
+$excerpt = elgg_get_excerpt($excerpt);
 
 echo elgg_view('river/elements/layout', array(
 	'item' => $vars['item'],
-	'message' => 'foo',
+	'message' => $excerpt,
 ));
