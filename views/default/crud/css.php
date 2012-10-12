41594300fa0f8f1b57d207e35b9b4d69428f4c76
Sem <sembrestels@riseup.net>
2012-10-12 17:43:09 +0200

    Moved some CSS from assemblies to here.

--- views/default/crud/css.php
+++ views/default/crud/css.php
@@ -12,4 +12,13 @@
 	margin-top: 20px;
 }
 
+div.crud-children > h3 {
+	margin-bottom: 0.2em;
+}
+div.crud-children > h4 {
+	padding: 0.5em;
+}
 
+div.crud-children > .elgg-list {
+	margin-left: 2em;
+}
